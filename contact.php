0a3452b58cbaf026a464ed4b266183000c820e16
Priscilla Barbosa <priscillabarbosa@outlook.com>
2025-02-15 15:25:10 -0300

    criando o setup do projeto

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 include_once("templates/header.php");
 ?>
 <body>
-    <h1>Olá Mundo!</h1>    
-<?php
+    <h1>Página de contato</h1>    
+    <?php
 include_once("templates/footer.php");
 ?>
---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>Priscilla Barbosa &copy; 2025</p>
+</footer>
+</body>
+</html>
